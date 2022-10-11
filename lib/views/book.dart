6dbedacdf86080d491df841d9067ddb2f706c2a4
konmai69 <102503235+konmai69@users.noreply.github.com>
2022-10-11 14:16:03 +0700

    Add dummy text forms and register button

--- lib/views/book.dart
+++ lib/views/book.dart
@@ -12,7 +12,70 @@ class _BookingFormState extends State<BookingForm>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Booking Form")),
+        appBar: AppBar(
+            title: Text("Booking Form")
+        ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          child: Column(
+            children: [
+
+              SizedBox(height:32),
+
+              TextFormField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  prefixIcon: Icon(Icons.person)
+                )
+              ),
+
+              SizedBox(height:32),
+
+              TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Icon(Icons.email)
+                  )
+              ),
+
+              SizedBox(height:32),
+
+              TextFormField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                      labelText: "Phone Number",
+                      prefixIcon: Icon(Icons.phone)
+                  )
+              ),
+
+              SizedBox(height:32),
+
+              TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      labelText: "City",
+                      prefixIcon: Icon(Icons.location_city)
+                  )
+              ),
+
+              SizedBox(height:32),
+
+              ElevatedButton.icon(
+                onPressed: () {
+
+                },
+                icon: Icon(Icons.app_registration),
+                label: Text("Register")
+              )
+
+            ],
+          )
+        )
+
+      )
     );
   }
 
